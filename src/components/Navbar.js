3b87020e8ded4564c6eb62279393d797e16e0752
Sankar4595 <95sankar@gmail.com>
2023-04-28 00:16:48 +0530

    navigate

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
 
 const Nav_Bar = () => {
-
+  const history = useNavigate();
   return <>
     <Navbar fixed="top">
       <Container>
@@ -10,10 +11,10 @@ const Nav_Bar = () => {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link style={{color:"white",fontSize:'1.5rem'}} href="/">Home</Nav.Link>
-            <Nav.Link style={{color:"white",fontSize:'1.5rem'}} href="/about">About</Nav.Link>
-            <Nav.Link style={{color:"white",fontSize:'1.5rem'}} href="/project">Project</Nav.Link>
-            <Nav.Link style={{color:"white",fontSize:'1.5rem'}} href="/contact">Contact</Nav.Link>
+            <Nav.Link style={{color:"white",fontSize:'1.5rem'}} onClick={()=>history('/')}>Home</Nav.Link>
+            <Nav.Link style={{color:"white",fontSize:'1.5rem'}} onClick={()=>history('/about')}>About</Nav.Link>
+            <Nav.Link style={{color:"white",fontSize:'1.5rem'}} onClick={()=>history('/project')}>Project</Nav.Link>
+            <Nav.Link style={{color:"white",fontSize:'1.5rem'}} onClick={()=>history('/contact')}>Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
