746d339ef14087646eb844cf92ce6ddcf3ed09eb
sankar <95sankar@gmail.com>
2024-06-03 20:19:57 +0530

    ui fixs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import Home from './routes/Home';
-import About from './routes/About';
-import Contact from './routes/Contact';
-import Project from './routes/Project';
-import { Route, Routes } from 'react-router-dom';
-import Nav_Bar from './components/Navbar';
-
+import { Route, Routes, useLocation, Navigate } from "react-router-dom";
+import React, { Suspense } from "react";
+import {
+  NotFound,
+  Loader,
+  ScrollToTop,
+  ProjectDetails,
+  Navbar,
+  Footer,
+  BlogDetail,
+  BlogIndex,
+  MernBlogRepair,
+  EmployeeSalaryManagementRepair,
+} from "./components";
+import Blog from "./pages/Blog";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+const Home = React.lazy(() => import("./pages/Home"));
+const Project = React.lazy(() => import("./pages/Project"));
 
 function App() {
-  return <>
-    <Nav_Bar/>
-    <Routes>
-      <Route exact path='/' element={<Home/>}/>
-      <Route path='/about' element={<About/>}/>
-      <Route path='/contact' element={<Contact/>}/>
-      <Route path='/project' element={<Project/>}/>
-    </Routes>
-  </>
+  const location = useLocation();
+  const isFalse = location.pathname.includes("404");
+  return (
+    <>
+      <ScrollToTop />
+      {isFalse || <Navbar />}
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/project" element={<Project />} />
+          <Route path="/project/:id" element={<ProjectDetails />} />
+          <Route path="/contact" element={<Contact />} />
+          {/* <Route path="/blog" element={<Blog />}>
+            <Route index element={<BlogIndex />} />
+            <Route path=":blog_path" element={<BlogDetail />} />
+          </Route> */}
+          <Route path="/404" element={<NotFound />} />
+          <Route path="*" element={<Navigate replace to="/404" />}></Route>
+          {/* <Route path="/mern-blog" element={<MernBlogRepair />} /> */}
+          {/* <Route
+            path="/employee-salary-management"
+            element={<EmployeeSalaryManagementRepair />}
+          /> */}
+        </Routes>
+      </Suspense>
+      {isFalse || <Footer />}
+    </>
+  );
 }
 
 export default App;
